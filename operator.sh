3fc0b6d9e171f3f31d29234df8d92ba0c1e4124a
Aneesh Puttur <aneeshputtur@gmail.com>
2019-06-10 15:16:56 -0400

    Update operator.sh

--- operator.sh
+++ operator.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 source="https://github.com/operator-framework/community-operators.git"
 OPERATOR_DIRECTORY="$HOME/tigeroperators"
+ACTION=SETUP
 #export OPERATOR_TYPE="community-operators" 
 #export OPERATOR_NAME="NONAMEOPERATOR"
 #export BRANCH_NAME="master"
@@ -8,42 +9,16 @@ OPERATOR_DIRECTORY="$HOME/tigeroperators"
 #export AUTH_TOKEN=""
 #export PACKAGE_NAME=""
 
-
-helpFunction()
-{
-   echo ""
-   echo "Usage: $0 -o parameterA -p parameterB -b parameterC  -h parameterh"
-   echo -e "\t-o , operator you are planning to test"
-   echo -e "\t-p pull id from Pull Request"
-   echo -e "\t-b brnachname to pull from "
-   echo -e "\t -h Help command"
-   exit 1 # Exit script after printing help
+error(){
+    msg=$1
+    printf "Error occured %s" "$msg"
+    echo "$1"
 }
-helpTestFunction()
-{ 
-    echo "Enter your quay username"
-    read QUAY_NAMESPACE
-    
-    PACKAGE_NAME="$(cat $OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/*package* | grep packageName | cut -f 2 -d' ')"
-    PACKAGE_VERSION="$(cat $OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/*package* | grep currentCSV | cut -d'.' -f2- | cut -d'v' -f2- )"
-    
-    echo "
-       TEST OPTIONS   
-       -----------------------------------------------------------------------------------------------
-        1 - GET Quay Token :  $OPERATOR_DIRECTORY/$OPERATOR_NAME/operator-courier/scripts/get-quay-token
-         ( Then set it to export QUAY_TOKEN=""basic abcdefghijkl=="")
-
-        2 - LINTING
-       
-         operator-courier verify --ui_validate_io $OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME
-         
-
-        3 - QUAY PUSHING
-         operator-courier push \""$OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME"\" \""$QUAY_NAMESPACE"\" \""$PACKAGE_NAME"\" \""$PACKAGE_VERSION"\" \""$QUAY_TOKEN"\"
-         
-         -----------------------------------------------------------------------------------------------
-         "
-    if [ $OPERATOR_TYPE == "upstream-community-operators" ] 
+#*********************************************
+# Print kubectl commands to isntall marketplace and olm required for testing "
+#*********************************************
+olminstallFunc(){
+    if [ "$OPERATOR_TYPE" = "upstream-community-operators" ] 
     then
        echo "  
        INSTALL OLM
@@ -55,46 +30,279 @@ helpTestFunction()
        ----------------------  
        (find it here $OPERATOR_DIRECTORY/$OPERATOR_NAME/)
 
-       kubectl apply -f operator-marketplace/deploy/upstream/
+       kubectl apply -f $OPERATOR_NAME/operator-marketplace/deploy/upstream/
 
        "
     fi 
-         
+}
+#*********************************************
+# Sets up operator  1. Creating folder , 2) cloning git 3) fetching PR 4)  and 5) switching to branch
+#*********************************************
+setupOperatorFunction(){
+    # Begin script in case all parameters are correct
+    if [ -d "$OPERATOR_DIRECTORY" ] 
+    then
+        printf "Directory %s exists.\n" "$OPERATOR_DIRECTORY"
+    else
+        printf "Error: Directory %s does not exists.\n" "$OPERATOR_DIRECTORY"
+        read -p "Do you want to create directory $OPERATOR_DIRECTORY? " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+        # do dangerous stuff
+        mkdir "$OPERATOR_DIRECTORY"
+        print " Directory $OPERATOR_DIRECTORY created.\n"
+        else
+             error "Exiting ......."
+             return
+        fi
+    fi
+
+    if [ -d "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME" ] 
+    then
+        printf "%s/%s ... cleaning up\n" "$OPERATOR_DIRECTORY" "$OPERATOR_NAME"
+        rm -rf "$OPERATOR_DIRECTORY/$OPERATOR_NAME"
+    fi
+    printf "Directory %s does not exists. Creating new.\n" "$OPERATOR_NAME"
+    mkdir "$OPERATOR_DIRECTORY/$OPERATOR_NAME"
+    printf "Directory %s/%s created.\n" "$OPERATOR_DIRECTORY" "$OPERATOR_NAME"
+
+    #Begin git  clone and PR's
+    cd "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/ || exit
+    git clone $source
+    cd "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/community-operators || exit 
+    git fetch origin pull/"$PULL_ID"/head:"$BRANCH_NAME"
+    git checkout "$BRANCH_NAME"
+
+
+    if [ "$OPERATOR_TYPE" = "upstream-community-operators" ]
+    then
+        printf "Cloning other required repo\n"
+        cd "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/ || exit
+        git clone https://github.com/operator-framework/operator-marketplace.git
+        git clone https://github.com/operator-framework/operator-courier.git
+        git clone https://github.com/operator-framework/operator-lifecycle-manager.git
         
+        olminstallFunc
+    else
+        printf "Cloning other required repo (operator courier)\n"
+        cd "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/ || exit
+        git clone https://github.com/operator-framework/operator-courier.git
+    fi
+
+    printf " *----------------------------------- Done -----------------------------------*\n"
+    printf "Make sure you run [pip3 install operator-courier].\n"
+    ls "$OPERATOR_DIRECTORY" 
+    printf "cd %s/%s/%s\n" "$OPERATOR_DIRECTORY" "$OPERATOR_NAME" "$OPERATOR_TYPE"
+    cd "$OPERATOR_DIRECTORY" || exit
 }
 
-operatorSetup(){
- echo "select operator type 1=comunity 2=upstream for  fetching PR or use 3 for testing help."
+#*********************************************
+# Menu option to choose operator type 1) Community or 2) Upstream Community
+#*********************************************
+newoperatorType(){
+     echo "Select operator type 1=comunity 2=upstream for  fetching PR."
+    selection=
+
+        echo "
+        CHOOSE OPERATOR TYPE
+        -----------------------------
+        1 - Community Operators
+        2 - Upstream Community Operators
+        -----------------------------
+    "
+    while :
+    do
+        echo "----------------------------"
+        echo -n "Select Operator Type (1/2): "
+        read selection
+        echo ""
+        case $selection in
+            1 ) export OPERATOR_TYPE="community-operators" 
+                break
+                ;;
+            2 ) export OPERATOR_TYPE="upstream-community-operators"
+                break
+                ;;
+            * ) echo "Please enter 1, pr 2"
+        esac
+    done
+    echo "You selectd operator type as $OPERATOR_TYPE"
+
+}
+#*********************************************
+# Menu option to read and set the operator name from the user
+#*********************************************
+newoperatorName(){
+    printf "OPERATOR NAME:\a"
+    read operatorname
+    export OPERATOR_NAME=$operatorname
+}
+
+#*********************************************
+# Entry poin to NEW Operator setup
+#*********************************************
+newOperatorSetup(){
+    ACTION=SETUP
+    newoperatorType
+    newoperatorName
+    printf "Enter Pull ID\n"
+    read pullid
+    printf "Enter Branch name (The branch name you want to create):\n"
+    read brnachname
+    export PULL_ID=$pullid
+    export BRANCH_NAME=$brnachname
+    setupOperatorFunction
+}
+
+void(){
+    return 
+}
+#*********************************************
+# Sets up env variables
+#*********************************************
+testVariableSetup(){
+
+    if [ -z "$OPERATOR_TYPE" ]
+    then
+      newoperatorType
+    else
+     printf "Using operator type  $OPERATOR_TYPE"  
+    fi
+    if [ -z "$OPERATOR_NAME" ]
+    then
+      newoperatorName
+    else
+       printf " Do you want to set up new or use existing operator\n"
+       echo  "****************** $OPERATOR_NAME ***********************\a "
+
     selection=
 
         echo "
         OPERATOR TYPE MENU
-        1 - Community Operators
-        2 - Upstream Community Operators
+        1 - Use
+        2 - New
     "
-        echo -n "Enter selection: "
-        read selection
+    while :
+    do
+        echo -r "Enter selection: "
+        read selection 
         echo ""
         case $selection in
-            1 ) export OPERATOR_TYPE="community-operators" ;;
-            2 ) export OPERATOR_TYPE="upstream-community-operators" ;;
-            * ) echo "Please enter 1, pr 2"
+            1 ) void 
+                break 
+                ;;
+            2 ) newoperatorName
+                break
+                ;;
+            * ) echo "Please enter 1, or 2"
         esac
-    echo "You selectd operator type as $OPERATOR_TYPE"
-
-    echo "Enter OpertorName"
-    read operatorname
-    export OPERATOR_NAME=$operatorname
-    echo "Enter Pull ID"
-    read pullid
-    export PULL_ID=$pullid
-    echo "Enter PR branch name"
-    read branchname
-    export BRANCH_NAME=$branchname
-    setupOperatorFunction
+    done
     
+    fi  
+       
+}
+setupQuay(){
+
+if [ ! -z "$QUAY_TOKEN" ]
+then
+    echo "quay token: $QUAY_TOKEN"
+    echo "Quay token already present, if you want new token clear env variable QUAY_TOKEN and run this again."
+    echo -n "Username: "
+    read QUAY_NAMESPACE
+
+    
+else
+    echo -n "Username: "
+    read QUAY_NAMESPACE
+    echo -n "Password: "
+    read -s PASSWORD
+    echo
+
+
+    quayout=$(curl -H "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '
+    {
+        "user": {
+            "username": "'"${QUAY_NAMESPACE}"'",
+            "password": "'"${PASSWORD}"'"
+        }
+    }' 2>/dev/null)
+
+    export QUAY_TOKEN=$(echo "$quayout" | jq '.token')
+
+    if [ -z "$QUAY_TOKEN" ]
+    then 
+    printf "Error  logging  to quay %s\n" "$error"
+    fi
+fi
+
 }
 
+#*********************************************
+# S Printing test commands 
+#*********************************************
+helpTestFunction(){ 
+    ACTION=HELP
+    useexisting=$1
+    if [ "$useexisting" = 0 ]
+    then   
+        testVariableSetup
+        echo "$OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/"
+        if [ ! -d "$OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/" ] 
+        then
+             printf "****************** $OPERATOR_NAME ***********************\n"
+             error "Error: Looks like you do not have the operator setup for $OPERATOR_NAME"
+             return
+        fi
+        
+        if  [ -e  "$OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/$OPERATOR_NAME.package.yaml" ]
+        then
+                printf "Checking for OLM BUNDLE... exists \n"
+        else
+            printf "***************************************************************************************\n"
+             error " OLM  bundle does not exists for oeprator $OPERATOR_NAME , please setup operator first. \n"
+             return
+        fi
+    fi
+    
+    setupQuay
+    if [ -z "$QUAY_TOKEN" ]
+    then 
+     printf "Couldn't get quay login.\n"
+     QUAY_TOKEN="Couldn't get quay login"
+    fi
+    
+    if  [ ! -e  "$OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/$OPERATOR_NAME.package.yaml" ]
+    then
+        error " No such file or directory $OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME/$OPERATOR_NAME.package.yaml"
+        return
+    else
+        PACKAGE_NAME="$(cat "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/community-operators/$OPERATOR_TYPE/"$OPERATOR_NAME"/*package* | grep packageName | cut -f 2 -d' ')"
+        PACKAGE_VERSION="$(cat "$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/community-operators/$OPERATOR_TYPE/"$OPERATOR_NAME"/*package* | grep currentCSV | cut -d'.' -f2- | cut -d'v' -f2- )"
+    fi
+
+
+    echo "
+       TEST OPTIONS   
+       -----------------------------------------------------------------------------------------------
+        1 - LINTING
+       
+         operator-courier verify --ui_validate_io $OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators/$OPERATOR_TYPE/$OPERATOR_NAME
+         
+
+        2 - QUAY PUSHING
+         operator-courier push \"""$OPERATOR_DIRECTORY"/"$OPERATOR_NAME"/community-operators/$OPERATOR_TYPE/"$OPERATOR_NAME""\" \"""$QUAY_NAMESPACE""\" \"""$PACKAGE_NAME""\" \"""$PACKAGE_VERSION""\" $QUAY_TOKEN
+         
+         -----------------------------------------------------------------------------------------------
+         "
+    
+    olminstallFunc
+        
+}
+
+#*********************************************
+# Slapsh screen shown at the start
+#*********************************************
 startupMenuFunction(){
     echo "select operator setup option or test command"
     selection=
@@ -104,71 +312,29 @@ startupMenuFunction(){
         2 - Print testing commands
         
     "
-        echo -n "Enter selection: "
-        read selection
+    while :
+    do
+        echo -n "Enter your selection (1/2): "
+        read  selection
         echo ""
         case $selection in
-            1 ) operatorSetup ;;
-            2 ) helpTestFunction ;;
+            1 ) setup 
+                break 
+                ;;
+            2 ) helpTestFunction 0 
+                break
+                ;;
             * ) echo "Please enter 1 or 2"
         esac
+    done
 
 }
+
+setup(){
+    newOperatorSetup
+    helpTestFunction 1
+}
+
 
 startupMenuFunction
 
-setupOperatorFunction(){
-    # Begin script in case all parameters are correct
-    if [ -d $OPERATOR_DIRECTORY ] 
-    then
-        echo "Directory OPERATOR_DIRECTORY exists" 
-    else
-        echo "Error: Directory OPERATOR_DIRECTORY does not exists."
-        read -p "Do you want to create directory OPERATOR_DIRECTORY? " -n 1 -r
-        echo    # (optional) move to a new line
-        if [[ $REPLY =~ ^[Yy]$ ]]
-        then
-        # do dangerous stuff
-        mkdir "$OPERATOR_DIRECTORY"
-        echo " Directory $OPERATOR_DIRECTORY created."
-        else
-        exit 1
-        fi
-    fi
-
-    if [ -d $OPERATOR_DIRECTORY/$OPERATOR_NAME ] 
-    then
-        echo "$OPERATOR_DIRECTORY/$OPERATOR_NAME ... cleaning up" 
-        rm -rf $OPERATOR_DIRECTORY/$OPERATOR_NAME 
-    fi
-    echo "Error: Directory $OPERATOR_NAME does not exists."
-    mkdir "$OPERATOR_DIRECTORY/$OPERATOR_NAME"
-    echo " Directory $OPERATOR_DIRECTORY/$OPERATOR_NAME created."
-
-    #Begin git  clone and PR's
-    cd $OPERATOR_DIRECTORY/$OPERATOR_NAME/
-    git clone $source
-    cd $OPERATOR_DIRECTORY/$OPERATOR_NAME/community-operators 
-    git fetch origin pull/$PULL_ID/head:$BRANCH_NAME
-    git checkout $BRANCH_NAME
-
-
-    if [ $OPERATOR_TYPE == "upstream-community-operators" ]
-    then
-        echo "pulling other required repo"
-        cd $OPERATOR_DIRECTORY/$OPERATOR_NAME/
-        git clone https://github.com/operator-framework/operaoperator-courier push OPERATOR_DIR QUAY_NAMESPACE PACKAGE_NAME PACKAGE_VERSION TOKEN
-        git clone https://github.com/operator-framework/operaoperator-courier push OPERATOR_DIR QUAY_NAMESPACE PACKAGE_NAME PACKAGE_VERSION TOKEN
-        git clone https://github.com/operator-framework/operaoperator-courier push OPERATOR_DIR QUAY_NAMESPACE PACKAGE_NAME PACKAGE_VERSION TOKENit
-    else
-        echo "pulling other required repo (operator courier)"operator-courier push OPERATOR_DIR QUAY_NAMESPACE PACKAGE_NAME PACKAGE_VERSION TOKEN
-        cd $OPERATOR_DIRECTORY/$OPERATOR_NAME/
-        git clone https://github.com/operator-framework/operator-courier.git
-    fi
-
-    echo "----------------------------------- All set"
-    echo "make sure you run [pip3 install operator-courier]"
-    ls $OPERATOR_DIRECTORY 
-    echo "cd $OPERATOR_DIRECTORY/$OPERATOR_NAME/$OPERATOR_TYPE"
-    cd $OPERATOR_DIRECTORY
-}
